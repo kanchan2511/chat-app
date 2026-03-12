1873c4c734018e0b4d5f2a5a366cbef90a096a79
kanchan2511 <kanchankumawat2509@gmail.com>
2026-03-12 17:08:41 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ io.on("connection", (socket) => {
 })
 
 //middleware setup
-app.use(express.json({ limit: "4mb" }))
+app.use(express.json({ limit: "10mb" }))
 app.use(cors());
 
 app.use("/api/status", (req, res) => res.send("server is live"))
